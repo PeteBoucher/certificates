5fe7567732fef6e2ac61f134ac80b77fb771ebbf
Pete Boucher <petebouch@gmail.com>
2014-04-25 11:03:15 +0200

    another passing spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,10 +5,18 @@ set :environment, :test
 
 def app; Sinatra::Application; end
 
-describe "My Sinatra Application" do
+describe "Certificate Verification Application" do
   include Rack::Test::Methods
   it "should allow accessing the home page" do
     get '/'
     last_response.should be_ok
+  end
+
+  describe 'verify' do
+    it 'should find a certificate record by hash' do
+      hash = '1234randomstring5678'
+      Certificate.should_receive(:find_by_identifier).with(hash)
+      get 'verify/' + hash
+    end
   end
 end
